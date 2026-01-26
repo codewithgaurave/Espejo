275b5fedd8bc628f38b6277f1e73af2d47358016
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-01-26 21:24:22 +0530

    done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -79,7 +79,7 @@ const Login = () => {
       >
         {/* Branding Section */}
         <div className="text-center mb-8">
-          <div className="w-20 h-20 mx-auto mb-4 rounded-full flex items-center justify-center bg-white p-2 shadow">
+          <div className="w-40 h-40 mx-auto mb-4  flex items-center justify-center bg-white p-2 ">
             <img
               src="esj.png"
               alt="Query Logo"
